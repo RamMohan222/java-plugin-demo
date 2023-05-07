639e54eee57d0774aea0587cfea7132c5c3f92b5
RamMohan <ram222mohan@gmail.com>
2023-05-08 02:03:32 +0530

    Update PluginLoader.java

--- plugin-loader/src/main/java/com/plugin/loader/PluginLoader.java
+++ plugin-loader/src/main/java/com/plugin/loader/PluginLoader.java
@@ -5,6 +5,8 @@ import java.io.FileFilter;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 import java.util.Iterator;
 import java.util.ServiceLoader;
 
@@ -12,7 +14,7 @@ import com.plugin.publisher.IPublisher;
 
 public class PluginLoader {
 
-	private static Iterator<IPublisher> extendClasspath(File pluginsDir) throws IOException {
+	public static Iterator<IPublisher> extendClasspath(File pluginsDir) throws IOException {
 		File[] jarFiles = pluginsDir.listFiles(new FileFilter() {
 			public boolean accept(File file) {
 				return file.getPath().toLowerCase().endsWith(".jar");
@@ -23,10 +25,29 @@ public class PluginLoader {
 			urls[i] = jarFiles[i].toURI().toURL();
 		}
 		URLClassLoader ucl = new URLClassLoader(urls);
+		
+		// It extended the current current class loader context
+		// URLClassLoader ucl = new URLClassLoader(urls, getContextClassLoader());
+		
 		ServiceLoader<IPublisher> sl = ServiceLoader.load(IPublisher.class, ucl);
 		System.out.println(sl.findFirst().isPresent() ? sl.findFirst().get() : null);
 		return sl.iterator();
 	}
+	
+	/**
+         * Wraps <code>Thread.currentThread().getContextClassLoader()</code> into a doPrivileged block if security manager is present
+         */
+        private static ClassLoader getContextClassLoader() {
+            if (System.getSecurityManager() == null) {
+                return Thread.currentThread().getContextClassLoader();
+            } else {
+                return AccessController.doPrivileged(new PrivilegedAction<ClassLoader>() {
+                    public ClassLoader run() {
+                        return Thread.currentThread().getContextClassLoader();
+                    }
+                });
+            }
+        }
 
 	public static void main(String[] args) {
 		try {
